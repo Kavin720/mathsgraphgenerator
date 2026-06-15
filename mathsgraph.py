57445845f5115e6307dc98ac94391be91e07d783
Kavin720 <selvankavin10@gmail.com>
2026-06-15 17:48:08 +0800

    Update mathsgraph.py

--- mathsgraph.py
+++ mathsgraph.py
@@ -7,23 +7,46 @@ x = sp.symbols('x')
 
 st.title("📈 Function Graph Plotter")
 
-st.write("Enter a function of x (example: x**2 + 3*x)")
+st.write("Enter function (use ** for power, * for multiplication, / for division, + for addition and - for subraction)")
 
 user_input = st.text_input("Function")
+choice = st.selectbox("Range", ["Small", "Medium", "Large", "Custom"])
 
-if user_input:
 
+# -------------------------
+# RANGE SELECTION
+# -------------------------
+if choice == "Small":
+    xmin, xmax = -10, 10
+
+elif choice == "Medium":
+    xmin, xmax = -50, 50
+
+elif choice == "Large":
+    xmin, xmax = -500, 500
+
+else:
+    xmin = st.number_input("Lower bound", value=-10, step=1)
+    xmax = st.number_input("Upper bound", value=10, step=1)
+
+
+# -------------------------
+# VALIDATION FIRST
+# -------------------------
+if xmin >= xmax:
+    st.error("Invalid range: lower bound must be less than upper bound")
+
+elif user_input == "":
+    st.warning("Enter a function first")
+
+else:
     try:
-        # convert to sympy expression
+        # convert function
         expr = sp.sympify(user_input)
 
-        # convert sympy → numpy function
         f = sp.lambdify(x, expr, "numpy")
 
-        # generate x values
-        x_vals = np.linspace(-10, 10, 400)
-
-        # compute y values
+        x_vals = np.linspace(xmin, xmax, 400)
         y_vals = f(x_vals)
 
         # plot
@@ -36,4 +59,4 @@ if user_input:
         st.pyplot(fig)
 
     except:
-        st.error("❌ Invalid function. Use ** to represent power, * to represent multiplication, + to represent addition and - to represent subraction")
+        st.error("❌ Invalid function. Use ** for power, * for multiplication, / for division, + for addition and - for subraction ")
